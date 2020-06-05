7d3e29818123a91984bee1109696ea40d2108e75
DESKTOP-HJQGR3I\Antonio <tonio.iuele@gmail.com>
2020-06-05 18:47:36 +0200

    rilascio modifiche

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
@@ -2,17 +2,25 @@ package com.in28minutes.rest.webservices.restfulwebservices.user;
 
 import java.util.Date;
 
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(description="All details about the user.")
 public class User {
 
 	private Integer id;
 
+	@NotNull
 	@Size(min = 2, message = "Name should have at least 2 characters")
+	@ApiModelProperty("Name should have at least 2 characters")
 	private String name;
 	
 	@Past
+	@ApiModelProperty(notes="Birth date should be in the past")
 	private Date birthDate;
 	
 	protected User() {}
